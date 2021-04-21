8d7d22afc0cbce57a2bf57fec11ee122f1c04b41
Shaheer Hassan <shaheerhassan@live.com>
2021-04-22 00:52:35 +0500

    sign in and sign up implemented

--- app/src/main/java/com/example/brobot/LoginActivity.java
+++ app/src/main/java/com/example/brobot/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.brobot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -7,15 +8,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     TextView signup;
     Button btnSignIn;
-
+    EditText email, password;
+    FirebaseAuth auth;
+    public static final String serverAddress = "http://39.32.126.18:9001";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
         else{
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
+
 
         assignViews();
 
@@ -48,14 +60,44 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         btnSignIn=findViewById(R.id.loginbtnSignIn);
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        auth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.loginetEmail);
+        password = findViewById(R.id.loginetPassword);
+
+
+       /* btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(LoginActivity.this,chatActivity.class);
                 startActivity(intent);
             }
+        });*/
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_email)) {
+                    Toast.makeText(LoginActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(txt_email, txt_password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(LoginActivity.this, chatActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Authentication Failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
         });
-
-
     }
 }
--- app/src/main/java/com/example/brobot/SignUpActivity.java
+++ app/src/main/java/com/example/brobot/SignUpActivity.java
@@ -1,35 +1,173 @@
 package com.example.brobot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
     TextView signIn;
+    EditText username, email, password;
+    Button btn_register;
+    FirebaseAuth auth;
+    DatabaseReference reference;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        assignViews();
+        auth = FirebaseAuth.getInstance();
+        signIn = findViewById(R.id.signuptvSignIn);
 
+        //  username = findViewById(R.id);
+        password = findViewById(R.id.signupetPassword);
+        email = findViewById(R.id.signupetEmail);
+        btn_register = findViewById(R.id.signupbtnSignUp);
 
-    }
-
-    private void assignViews(){
-
-        signIn=findViewById(R.id.signuptvSignIn);
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(SignUpActivity.this,LoginActivity.class);
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_username = username.getText().toString();
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_email)) {
+                    Toast.makeText(SignUpActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 6) {
+                    Toast.makeText(SignUpActivity.this, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show();
+                } else {
+                    //registers user on Firebase and on Django server
+                    register(txt_username, txt_email, txt_password);
+
+                }
+            }
+        });
+
+    }
+
+    void PostUser(final String fuser) {
+
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
+        String url = LoginActivity.serverAddress + "/api/users/register/";
+        final String userID = fuser;
+        Log.d("UID", userID);
+        JSONObject postparams = new JSONObject();
+        try {
+            Log.d("UID", userID);
+            postparams.put("user_hash", userID);
+
+        } catch (JSONException e) {
+            //Toast t = new Toast();
+            e.printStackTrace();
+        }
+        JsonObjectRequest MyJsonRequest = new JsonObjectRequest(Request.Method.POST, url, postparams, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+
+
+                    if (!response.isNull("user_hash")) {
+                        Log.d("response", response.get("user_hash").toString());
+                        Toast.makeText(SignUpActivity.this, "User Registered on Server", Toast.LENGTH_SHORT).show();
+                    } else
+                        Toast.makeText(SignUpActivity.this, "User couldn't be registered on server", Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        MyRequestQueue.add(MyJsonRequest);
+        //set timeout to 15 seconds
+        MyJsonRequest.setRetryPolicy(new DefaultRetryPolicy(
+                15000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+    }
+
+    private void register(final String username, final String email, final String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser fireBaseUser = auth.getCurrentUser();
+                            assert fireBaseUser != null;
+                            String userid = fireBaseUser.getUid();
+
+                            PostUser(userid);
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+                            Log.d("USERID", userid);
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username);
+                            hashMap.put("imageURL", "default");
+
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(SignUpActivity.this, SignUpActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "You cannot register with Email or Password", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+                });
+
     }
 
 }
